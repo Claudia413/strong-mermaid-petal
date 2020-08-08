299bd12da634d823ec95a3817435c3fa14e23616
Claudia <cmjengelsman@gmail.com>
2020-08-08 18:08:25 +1200

    Retrieve about page content from Prismic

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,36 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>{{fields.title}}</h1>
+     <prismic-rich-text :field="fields.richContent"/>
+     <prismic-image :field="fields.photo"/>
   </div>
 </template>
+
+
+<script>
+export default {
+  data () {
+    return {
+      fields: {
+        title: null,
+        richContent: null,
+        photo: null
+      }
+    };
+  },
+  methods: {
+    getContent () {
+      this.$prismic.client.getSingle('about')
+        .then((document) => {
+          this.fields.title = document.data.title[0].text;
+          this.fields.richContent = document.data.content;
+          this.fields.photo = document.data.photo;
+        })
+    }
+  },
+  created () {
+    this.getContent();
+    console.log('hi', this.fields);
+  }
+}
+</script>
