27be9a1e515871210cfb499981ce80858299b554
Claudia <cmjengelsman@gmail.com>
2020-08-08 17:47:26 +1200

    Install and set up Prismic CMS

---
+++ src/link-resolver.js
@@ -0,0 +1,14 @@
+export default function (doc) {
+    // Return the path depending on Prismic Document's type
+    // If it is a Single Custom Type with the API ID of "home"
+    if (doc.type === 'about') {
+        return '/about';
+    }
+
+    if (doc.type === 'blogpost') {
+        return '/blog/' + doc.uid;
+    }
+
+    // Default to the root
+    return '/';
+}
