260838e5a89be52905a6a6df7ebcbdf01aef2f61
Claudia <cmjengelsman@gmail.com>
2020-08-08 17:39:43 +1200

    Install and setup Prismic for CMS

--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import PrismicVue from '@prismicio/vue'
+import linkResolver from './link-resolver'
 
 Vue.config.productionTip = false;
+
+const accessToken = process.env.VUE_APP_PRISMIC_API_KEY;
+
+Vue.use(PrismicVue, {
+  endpoint: window.prismic.endpoint,
+  linkResolver,
+  apiOptions: {
+    accessToken
+  }
+});
 
 new Vue({
   router,
