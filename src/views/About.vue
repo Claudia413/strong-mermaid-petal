57678e44a61092fef8c1513cda02b79459c3912e
Claudia <cmjengelsman@gmail.com>
2020-08-17 17:01:02 +1200

    Add styling for the about page including responsiveness tweaks

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,11 +1,60 @@
 <template>
   <div class="about">
-    <h1>{{fields.title}}</h1>
-     <prismic-rich-text :field="fields.richContent"/>
-     <prismic-image :field="fields.photo"/>
+    <div class="content">
+    <section class="text">
+      <h1>{{fields.title}}</h1>
+      <prismic-rich-text :field="fields.richContent"/>
+    </section>
+    <prismic-image :field="fields.photo" class="img"/>
+    </div>
   </div>
 </template>
 
+<style lang="scss" scoped>
+.about {
+  display: flex;
+  align-content: center;
+  justify-content: center;
+  background-color: #fcfcfc;
+}
+.content {
+  max-width: 1600px;
+  padding: 80px;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-template-areas: "text img";
+  column-gap: 80px;
+  @media only screen and (max-width: 900px) {
+    padding: 80px 48px;
+    column-gap: 64px;
+  }
+  @media only screen and (max-width: 768px) {
+    grid-template-columns: 1fr;
+    grid-template-areas: "text"
+                          "img";
+  }
+}
+
+.text {
+  grid-area: text;
+  max-width: 100%;
+  font-size: 18px;
+  padding: 20px;
+  h1 {
+    margin-top: 64px;
+    margin-bottom: 24px;
+  }
+}
+
+.img {
+  grid-area: img;
+  max-width: 100%;
+  margin: 0 auto;
+  @media only screen and (max-width: 768px) {
+    margin: 0 24px;
+  }
+}
+</style>
 
 <script>
 export default {
