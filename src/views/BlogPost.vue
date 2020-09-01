8e71f14833d7d72375455fb580d928e15ca0b2c6
Claudia <cmjengelsman@gmail.com>
2020-09-01 17:13:22 +1200

    Set up overview page for categories with styling, make it use state first
    to prevent needlessly requesting content from prismic again

--- src/views/BlogPost.vue
+++ src/views/BlogPost.vue
@@ -109,7 +109,7 @@ export default {
 };
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 .blog {
 	padding: 80px;
 	display: flex;
--- src/views/Overview.vue
+++ src/views/Overview.vue
@@ -8,9 +8,20 @@
 			:image="blog.seo_image"
 			:url="'/category/' + blog.tag"
 			/>-->
-			<div v-for="(post, index) in blogpreviews" :key="'post-' + index" :blogId="post.uid" class="blog-post" >
-            <div> {{ post.data.blog_title[0].text}}</div>
-
+            <h2>Posts in {{blog.tag | capitalize}}</h2>
+            <div class="blogs">
+                <div v-for="(post, index) in blogpreviews" :key="'post-' + index" :blogId="post.uid" class="blog" >
+                    <router-link :to="'/blog/' + post.uid"><prismic-image :field="post.data.cover_image" class="cover-image" /></router-link>
+                    <p class="publish-date">{{ readableDate(post.first_publication_date) }} </p>
+                    <h3 class="title">
+                        <router-link :to="'/blog/' + post.uid">
+                            {{ post.data.blog_title[0].text}}
+                        </router-link> </h3>
+                    <p class="teaser">
+                        <prismic-rich-text :field="post.data.teaser" />
+                        <router-link :to="'/blog/' + post.uid"> Read more <ArrowRight :size="14"/></router-link>
+                    </p>
+                </div>
             </div>
 		</main>
 	</div>
@@ -20,6 +31,8 @@
 // import moment from "moment";
 import Navigation from "@/components/Navigation.vue";
 import { mapState } from 'vuex';
+import moment from 'moment';
+import ArrowRight from "mdi-vue/ArrowRight.vue";
 
 export default {
 	name: "Overview",
@@ -33,7 +46,8 @@ export default {
 		};
 	},
 	components: {
-		Navigation,
+        Navigation,
+        ArrowRight
 	},
 	methods: {
 		getContent(category, pageNumber) {
@@ -59,56 +73,117 @@ export default {
 					);
 					// response is the response object, response.results holds the documents
 				});
-		},
+        },
+        readableDate(date) {
+            return moment(date).format("MMMM Do, YYYY")
+        }
     },
-    computed: mapState({
+    computed: {
+        ...mapState({
         blogpreviews (state){
             return state['blogpreviews' + this.blog.tag]
             }
-        }),
+        })
+    },
 	created() {
-        this.getContent(this.$route.params.tagid, this.pageNumber);
+        if(this.$store.state['blogpreviews' + this.$route.params.tagid].length >= 1) {
+            this.blog.tag = this.$route.params.tagid;
+        } else {
+            this.getContent(this.$route.params.tagid, this.pageNumber);
+        }
+
 	},
 	beforeRouteUpdate(to, from, next) {
-		//   This prevents blog page not updating upon route change
-        this.getContent(to.params.tagid, 1);
-		next();
+        //   This prevents blog page not updating upon route change and checks if info is already retrieved from prismic
+        if(this.$store.state['blogpreviews' + to.params.tagid].length >= 1) {
+            this.blog.tag = to.params.tagid;
+            next();
+        } else {
+            this.getContent(to.params.tagid, 1);
+            next();
+        }
+
 	},
 };
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 .overview {
 	padding: 80px;
 	display: flex;
-	justify-content: center;
+    flex-direction: column;
+    justify-content: center;
+    max-width: 1200px;
+    margin: 0 auto;
 }
 
-article {
-	max-width: 1200px;
+h2 {
+    font-size: 18px;
+    line-height: 28px;
+    font-weight: 400;
+}
+
+.blogs {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: space-between;
+}
+
+.blog {
+    display: flex;
+    flex-direction: column;
+    max-width: 30%;
+    justify-content: flex-start;
+    @media only screen and (max-width: 768px) {
+		max-width: 45%;
+    }
+    a {
+        text-decoration: none;
+        color: inherit;
+    }
+}
+
+.cover-image {
+	width: 100%;
+	height: auto;
+	object-fit: cover;
 }
 
 .title,
 .publish-date {
-	text-align: center;
-	font-family: "Baskervville", Georgia, "Times New Roman", Times, serif;
+	text-align: left;
+    font-family: "Baskervville", Georgia, "Times New Roman", Times, serif;
+    margin: 0;
 }
 
 .title {
 	font-weight: 400;
-	font-size: 42px;
-	line-height: 50px;
+	font-size: 22px;
+    line-height: 28px;
+    margin: 12px 0;
 }
 
 .publish-date {
 	color: RGBA(20, 20, 20, 0.5);
 	font-size: 13px;
-	font-style: italic;
+    font-style: italic;
+    margin-top: 8px;
 }
 
-.blog-image {
-	width: 100%;
-	height: auto;
-	object-fit: cover;
+.teaser {
+    font-family: "Josefin Sans",
+        Arial,
+        sans-serif;
+        font-weight: 300;
+        font-size: 14px;
+        margin: 0;
+        line-height: 22px;
+    a {
+        transition: all 0.2s ease-in-out;
+        &:hover {
+            color: RGBA(20, 20, 20, 0.5);
+        }
+    }
 }
 </style>
