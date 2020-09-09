ea752667bf5caf14c3b6cc8cd828374f40e82328
Claudia <cmjengelsman@gmail.com>
2020-09-09 21:31:18 +1200

    Add coming back soon page as cover while website is being filled

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,11 @@
 import Vue from "vue";
 import store from "../store";
 import Router from "vue-router";
-import Home from "../views/Home.vue";
-import About from "../views/About.vue";
-import BlogPost from "../views/BlogPost.vue";
-import Overview from "../views/Overview.vue";
+// import Home from "../views/Home.vue";
+// import About from "../views/About.vue";
+// import BlogPost from "../views/BlogPost.vue";
+// import Overview from "../views/Overview.vue";
+import ComingSoon from "../views/ComingSoon.vue"
 
 Vue.use(Router);
 
@@ -12,30 +13,35 @@ const router = new Router({
   mode: "history",
   routes: [
     {
-      path: "/",
-      name: "Home",
-      component: Home
-    },
-    {
-      path: "/about",
-      name: "about",
-      component: About
-    },
-    {
-      path: "/blog/:uid",
-      name: "blog-post",
-      component: BlogPost
-    },
-    {
-      path: '/category/:tagid',
-      name: 'blog-category',
-      component: Overview,
-    },
-    {
       path: "*",
       name: "everything",
-      component: Home
-    }
+      component: ComingSoon
+    },
+    // {
+    //   path: "/",
+    //   name: "Home",
+    //   component: Home
+    // },
+    // {
+    //   path: "/about",
+    //   name: "about",
+    //   component: About
+    // },
+    // {
+    //   path: "/blog/:uid",
+    //   name: "blog-post",
+    //   component: BlogPost
+    // },
+    // {
+    //   path: '/category/:tagid',
+    //   name: 'blog-category',
+    //   component: Overview,
+    // },
+    // {
+    //   path: "*",
+    //   name: "everything",
+    //   component: Home
+    // }
   ],
   scrollBehavior(to, from, savedPosition) {
     if (savedPosition) {
---
+++ src/views/ComingSoon.vue
@@ -0,0 +1,42 @@
+<template>
+  <main class="comingsoon">
+    <div class="content">
+     <img src="../assets/Logo3.png" alt="the Bikes & I" class="logo" />
+     <h1>Coming back soon...</h1>
+    </div>
+  </main>
+</template>
+
+<script>
+
+
+</script>
+
+<style lang="scss" scoped>
+.comingsoon {
+  display: flex;
+  align-content: center;
+  justify-content: center;
+  background-color: #fcfcfc;
+  margin: 64px 0;
+  @media only screen and (max-width: 768px) {
+		margin: 40px 0;
+	}
+}
+.content {
+  max-width: 1200px;
+  padding: 80px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  img {
+    width: 360px;
+    height: auto;
+  }
+  h1 {
+    margin: 64px;
+  }
+}
+
+
+</style>
