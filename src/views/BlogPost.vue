95b29a0e4e965397ed5577b715b31e3ce77bc15c
Claudia <cmjengelsman@gmail.com>
2021-11-13 16:38:52 +1300

    Fix 1 linting problem, remove class from template tag

--- src/views/BlogPost.vue
+++ src/views/BlogPost.vue
@@ -25,35 +25,19 @@
           </template>
           <template v-else-if="slice.slice_type === 'picture'">
             <prismic-image :field="slice.primary.picture" class="blog-image" />
-            <prismic-rich-text
-              :field="slice.primary.caption"
-              class="blog-image-caption"
-            />
+            <prismic-rich-text :field="slice.primary.caption" class="blog-image-caption" />
           </template>
           <template v-else-if="slice.slice_type === 'text_with_image'">
-            <prismic-image
-              :field="slice.primary.image"
-              class="blog-image-next-text"
-            />
-            <prismic-rich-text
-              :field="slice.primary.text"
-              class="blog-text-next-image"
-            />
+            <prismic-image :field="slice.primary.image" class="blog-image-next-text" />
+            <prismic-rich-text :field="slice.primary.text" class="blog-text-next-image" />
           </template>
-          <template
-            v-else-if="slice.slice_type === 'ride_stats'"
-            class="ride-stats"
-          >
+          <template v-else-if="slice.slice_type === 'ride_stats'">
             <span class="ride-stat">
               <mdicon name="bike" class="ride-stat-icon" />
               {{ slice.primary.bike[0].text }}
             </span>
             <span class="ride-stat">
-              <mdicon
-                name="speedometer"
-                class="ride-stat-icon"
-                title="My tip"
-              />
+              <mdicon name="speedometer" class="ride-stat-icon" title="My tip" />
               {{ slice.primary.avg_speed[0].text }} km / hr
             </span>
             <span class="ride-stat">
@@ -62,12 +46,7 @@
             </span>
             <span class="ride-stat">
               <mdicon name="flagCheckered" class="ride-stat-icon" :h="24" />
-              <a
-                :href="slice.primary.strava_url[0].text"
-                target="_blank"
-                rel="noopener"
-                class="strava-link"
-              >
+              <a :href="slice.primary.strava_url[0].text" target="_blank" rel="noopener" class="strava-link">
                 See on Strava</a
               >
             </span>
@@ -107,202 +86,200 @@
 </template>
 
 <script>
-import moment from "moment";
-import Navigation from "@/components/Navigation.vue";
+  import moment from "moment";
+  import Navigation from "@/components/Navigation.vue";
 
-export default {
-  name: "blogPost",
-  data() {
-    return {
-      blog: {
-        title: [{ text: "" }],
-        labels: [],
-        prismicID: "",
-        uid: "",
-        first_publication_date: "",
-        seo_title: "",
-        seo_description: "",
-        seo_image: "",
-      },
-      slices: [],
-      slides: [],
-    };
-  },
-  components: {
-    Navigation,
-  },
-  methods: {
-    getContent(uid) {
-      this.$prismic.client.getByUID("blogpost", uid).then((document) => {
-        // Can't simply put the result in an empty blog object without console erros about undefined before its done loading. So we set them individually with starting empty objects and arrays
-        this.blog.title = document.data.blog_title;
-        this.blog.first_publication_date = moment(
-          document.first_publication_date
-        ).format("MMMM Do YYYY");
-        this.blog.labels = document.tags;
-        this.blog.seo_title = document.data.seo_title[0].text;
-        this.blog.seo_description = document.data.seo_description[0].text;
-        this.blog.seo_image = document.data.seo_image.url;
-        this.slices = document.data.body;
-        this.blog.prismicID = document.id;
-        var that = this;
-        this.slices.forEach(function (slice) {
-          // find carousel slice if present and create data the carousel plugin will accept
-          if (slice.slice_type === "carousel") {
-            that.slides = slice.items;
-          }
-        });
-      });
+  export default {
+    name: "blogPost",
+    data() {
+      return {
+        blog: {
+          title: [{ text: "" }],
+          labels: [],
+          prismicID: "",
+          uid: "",
+          first_publication_date: "",
+          seo_title: "",
+          seo_description: "",
+          seo_image: "",
+        },
+        slices: [],
+        slides: [],
+      };
     },
-  },
-  created() {
-    this.getContent(this.$route.params.uid);
-    this.blog.uid = this.$route.params.uid;
-  },
-  beforeRouteUpdate(to, from, next) {
-    //   This prevents blog page not updating upon route change
-    this.getContent(to.params.uid);
-    next();
-  },
-};
+    components: {
+      Navigation,
+    },
+    methods: {
+      getContent(uid) {
+        this.$prismic.client.getByUID("blogpost", uid).then((document) => {
+          // Can't simply put the result in an empty blog object without console erros about undefined before its done loading. So we set them individually with starting empty objects and arrays
+          this.blog.title = document.data.blog_title;
+          this.blog.first_publication_date = moment(document.first_publication_date).format("MMMM Do YYYY");
+          this.blog.labels = document.tags;
+          this.blog.seo_title = document.data.seo_title[0].text;
+          this.blog.seo_description = document.data.seo_description[0].text;
+          this.blog.seo_image = document.data.seo_image.url;
+          this.slices = document.data.body;
+          this.blog.prismicID = document.id;
+          var that = this;
+          this.slices.forEach(function (slice) {
+            // find carousel slice if present and create data the carousel plugin will accept
+            if (slice.slice_type === "carousel") {
+              that.slides = slice.items;
+            }
+          });
+        });
+      },
+    },
+    created() {
+      this.getContent(this.$route.params.uid);
+      this.blog.uid = this.$route.params.uid;
+    },
+    beforeRouteUpdate(to, from, next) {
+      //   This prevents blog page not updating upon route change
+      this.getContent(to.params.uid);
+      next();
+    },
+  };
 </script>
 
 <style lang="scss" scoped>
-.blog {
-  padding: 80px;
-  display: flex;
-  justify-content: center;
-}
-
-article {
-  max-width: 1200px;
-}
-
-.title,
-.publish-date {
-  text-align: center;
-  font-family: "Baskervville", Georgia, "Times New Roman", Times, serif;
-}
-
-.title {
-  font-weight: 400;
-  font-size: 42px;
-  line-height: 50px;
-}
-
-.publish-date {
-  color: RGBA(20, 20, 20, 0.5);
-  font-size: 13px;
-  font-style: italic;
-}
-.picture {
-  display: flex;
-  flex-direction: column;
-  padding: 0 10%;
-}
-.blog-image {
-  width: 100%;
-  height: auto;
-  object-fit: cover;
-  margin-top: 20px;
-}
-
-.text {
-  ul,
-  ol {
-    font-size: 16px;
+  .blog {
+    padding: 80px;
+    display: flex;
+    justify-content: center;
   }
-}
 
-.blog-image-caption {
-  font-size: 12px;
-  line-height: 20px;
-  padding-top: 8px;
-  margin-bottom: 20px;
-  p {
-    margin: 0;
-    padding: 0;
+  article {
+    max-width: 1200px;
   }
-}
 
-.ride_stats {
-  display: flex;
-  justify-content: space-between;
-  align-content: space-between;
-  margin: 64px 0;
-}
+  .title,
+  .publish-date {
+    text-align: center;
+    font-family: "Baskervville", Georgia, "Times New Roman", Times, serif;
+  }
 
-.ride-stat {
-  margin: 8px;
-  display: flex;
-  align-items: center;
-  &:nth-child(1) {
-    margin: 8px 0;
+  .title {
+    font-weight: 400;
+    font-size: 42px;
+    line-height: 50px;
   }
-  .ride-stat-icon {
-    margin-right: 8px;
-    height: 24px;
-  }
-  .strava-link {
-    cursor: pointer;
-    color: unset;
-  }
-  @media only screen and (max-width: 768px) {
+
+  .publish-date {
+    color: RGBA(20, 20, 20, 0.5);
     font-size: 13px;
+    font-style: italic;
   }
-}
-
-.text_with_image {
-  display: flex;
-  justify-content: space-between;
-  &.image-on-right {
-    flex-direction: row-reverse;
-    @media only screen and (max-width: 768px) {
-      flex-direction: column;
-    }
+  .picture {
+    display: flex;
+    flex-direction: column;
+    padding: 0 10%;
   }
-  .blog-image-next-text {
-    margin-top: 20px;
-    max-width: 48%;
+  .blog-image {
+    width: 100%;
     height: auto;
     object-fit: cover;
-  }
-  .blog-text-next-image {
-    max-width: 48%;
-    height: auto;
     margin-top: 20px;
   }
-  @media only screen and (max-width: 768px) {
-    flex-direction: column;
+
+  .text {
+    ul,
+    ol {
+      font-size: 16px;
+    }
+  }
+
+  .blog-image-caption {
+    font-size: 12px;
+    line-height: 20px;
+    padding-top: 8px;
+    margin-bottom: 20px;
+    p {
+      margin: 0;
+      padding: 0;
+    }
+  }
+
+  .ride_stats {
+    display: flex;
+    justify-content: space-between;
+    align-content: space-between;
+    margin: 64px 0;
+  }
+
+  .ride-stat {
+    margin: 8px;
+    display: flex;
+    align-items: center;
+    &:nth-child(1) {
+      margin: 8px 0;
+    }
+    .ride-stat-icon {
+      margin-right: 8px;
+      height: 24px;
+    }
+    .strava-link {
+      cursor: pointer;
+      color: unset;
+    }
+    @media only screen and (max-width: 768px) {
+      font-size: 13px;
+    }
+  }
+
+  .text_with_image {
+    display: flex;
+    justify-content: space-between;
+    &.image-on-right {
+      flex-direction: row-reverse;
+      @media only screen and (max-width: 768px) {
+        flex-direction: column;
+      }
+    }
     .blog-image-next-text {
-      max-width: 80%;
-      margin-left: 10%;
-      margin-bottom: 20px;
+      margin-top: 20px;
+      max-width: 48%;
+      height: auto;
+      object-fit: cover;
     }
     .blog-text-next-image {
-      max-width: 100%;
+      max-width: 48%;
+      height: auto;
+      margin-top: 20px;
     }
-  }
-}
-
-.carousel {
-  margin-top: 40px;
-  .item {
-    width: 100%;
-    height: 500px;
-    max-height: 800px;
-    object-fit: cover;
     @media only screen and (max-width: 768px) {
-      max-height: 320px;
-    }
-    img {
-      height: 100%;
+      flex-direction: column;
+      .blog-image-next-text {
+        max-width: 80%;
+        margin-left: 10%;
+        margin-bottom: 20px;
+      }
+      .blog-text-next-image {
+        max-width: 100%;
+      }
     }
   }
-  .custom-navigation {
-    background-color: rgba(255, 255, 255, 0.8);
-    border-radius: 50%;
-    cursor: pointer;
+
+  .carousel {
+    margin-top: 40px;
+    .item {
+      width: 100%;
+      height: 500px;
+      max-height: 800px;
+      object-fit: cover;
+      @media only screen and (max-width: 768px) {
+        max-height: 320px;
+      }
+      img {
+        height: 100%;
+      }
+    }
+    .custom-navigation {
+      background-color: rgba(255, 255, 255, 0.8);
+      border-radius: 50%;
+      cursor: pointer;
+    }
   }
-}
 </style>
